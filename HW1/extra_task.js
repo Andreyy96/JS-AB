b6be03e6b751b78b95138f08dcc0e35469b5b237
Andreyy96 <taurussilver777@gmail.com>
2023-11-21 12:52:33 +0200

    add hw1.html, task1-3.js and extra_task.js.

---
+++ HW1/extra_task.js
@@ -0,0 +1,7 @@
+let g = prompt('Name', '')
+let h = prompt('lastName' , '');
+let j = prompt('Year', '')
+
+console.log(g)
+console.log( h)
+console.log( j)
---
+++ HW1/task2.js
@@ -0,0 +1,9 @@
+let firstName = 'Андрей'
+let middleName = 'Бондаренко'
+let lastName = 'Николаёвич'
+
+let person1 = `${firstName} ${middleName} ${lastName}`
+let person2 = firstName + ' ' + middleName + ' ' + lastName
+
+console.log(person1)
+console.log(person2)
